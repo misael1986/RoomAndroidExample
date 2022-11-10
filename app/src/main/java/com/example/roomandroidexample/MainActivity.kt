ce5ee31673f25aaef6c58986b543e6b895469e1e
misael.perilla <gabelogan0167@hotmail.com>
2022-11-09 22:04:42 -0500

    Cambios para mejorar

--- app/src/main/java/com/example/roomandroidexample/MainActivity.kt
+++ app/src/main/java/com/example/roomandroidexample/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-
         floatingActionButton.setOnClickListener {
             val intent = Intent(this@MainActivity,AddUserActivity::class.java)
             startActivity(intent)
